4a02017c6066589391d43c86de5c7a0df3e65a14
SW-adachi <adachi@starryworsk.co.jp>
2024-02-14 15:49:18 +0900

    add dmx parameter

--- 04_dmx/src/renderer/src/sketch.ts
+++ 04_dmx/src/renderer/src/sketch.ts
@@ -26,12 +26,22 @@ export const sketch = (p: p5): void => {
 			const w = 0;
 			lightColorApi.refresh();
 			// ElectronのIPCでメインプロセスにDMXデータを送信する
-			window.electron.ipcRenderer.invoke("DmxSend", 1, [
-				params.lightColor.r,
-				params.lightColor.g,
-				params.lightColor.b,
-				w,
-			]);
+			// 0 - 255 の値を送信してください
+			// 1: pan
+			// 2: pan fine
+			// 3: tilt
+			// 4: tilt fine
+			// 7: red
+			// 8: green
+			// 9: blue
+			// 10: white
+			// 11 : dimmer
+			window.electron.ipcRenderer.invoke("DmxSend", 1, [params.lightColor.r]);
+			window.electron.ipcRenderer.invoke("DmxSend", 3, [params.lightColor.g]);
+			window.electron.ipcRenderer.invoke("DmxSend", 7, [params.lightColor.r]);
+			window.electron.ipcRenderer.invoke("DmxSend", 8, [params.lightColor.g]);
+			window.electron.ipcRenderer.invoke("DmxSend", 9, [params.lightColor.b]);
+			window.electron.ipcRenderer.invoke("DmxSend", 11, [255]);
 		});
 	};
 
